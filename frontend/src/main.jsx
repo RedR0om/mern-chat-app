6d7aa85143374e88222e655c52fb524c89ef467a
kelvindichoso <kelvind@apc.edu.ph>
2024-10-29 11:14:14 +0800

    4th part completed - socket io

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,19 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
+import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext.jsx";
+import { SocketContextProvider } from "./context/SocketContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <React.StrictMode>
     <BrowserRouter>
       <AuthContextProvider>
-        <App />
+        <SocketContextProvider>
+          <App />
+        </SocketContextProvider>
       </AuthContextProvider>
     </BrowserRouter>
-  </StrictMode>
+  </React.StrictMode>
 );
